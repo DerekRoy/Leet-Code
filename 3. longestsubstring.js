eca5c0ab35862b1007f9e99475cacf2e5c63670c
Derek Roy <Derek.Raymond.Roy@ibm.com>
2022-11-25 17:08:31 -0800

    update longest substring

--- 3. longestsubstring.js
+++ 3. longestsubstring.js
@@ -1,3 +1,21 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-var lengthOfLongestSubstring = function (s) {};
+// Faster than 66.55% of JS Submissions, memory usage less than 41.25% JS submissions on November 23, 2022
+var lengthOfLongestSubstring = function (s) {
+  let seen = {};
+  let max = 0;
+  let start = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+
+    if (seen[char] !== undefined) {
+      start = Math.max(max, seen[char] + 1);
+    }
+
+    seen[char] = i;
+    max = Math.max(max, i - start + 1);
+  }
+
+  return max;
+};
